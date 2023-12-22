c564c7414a50dc537de3a81a567a2bb733b4a3d7
Benjamin Maggi <benjamin.maggi@fravega.com.ar>
2023-12-22 13:54:54 -0300

    chore: code clean

--- src/utils/nextConfigVariables.ts
+++ src/utils/nextConfigVariables.ts
@@ -4,23 +4,9 @@ const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 const gtmId = serverRuntimeConfig?.gtmId
 
 let apiSearch = publicRuntimeConfig?.apiSearch
-// eslint-disable-next-line no-console
-console.log('window', 'defined');
-// eslint-disable-next-line no-console
-console.log('process', process.env);
-// eslint-disable-next-line no-console
-console.log('publicRuntimeConfig', publicRuntimeConfig);
- 
 // Para el SSR
 if (typeof window === 'undefined') {
-    
-    // eslint-disable-next-line no-console
-    console.log('window', 'undefined');
-    // eslint-disable-next-line no-console
-    console.log('process', process.env);
-    // eslint-disable-next-line no-console
-    console.log('serverRuntimeConfig', serverRuntimeConfig);
-    apiSearch = serverRuntimeConfig?.apiSearch
+  apiSearch = serverRuntimeConfig?.apiSearch
 }
 
 export {
