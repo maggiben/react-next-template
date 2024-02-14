25940b51aeb1a467f14c3e108ee9f85b9d70ef00
Benjamin Maggi <benjamin.maggi@fravega.com.ar>
2024-02-14 10:48:44 -0300

    chore: map marker

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,12 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { renderToStaticMarkup } from 'react-dom/server';
+import Image from 'next/image';
 import styled from "styled-components";
 import L from 'leaflet';
-// import { useMap } from 'react-leaflet/hooks'
+import locationIcon_bg from '@images/locationIcon_bg.svg';
 import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 
 const Container = styled.div`
-  /* border: 0.125rem solid black; */
   width: calc(100% - 2px);
   border: 0px;
   border-width: 1px;
@@ -25,57 +26,17 @@ interface IMapProps {
 
 const Map = ({latitude, longitude, marker}: IMapProps) => {
 
-  // const LeafIcon3 = new L.Icon({
-    // iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|abcdef&chf=a,s,ee00FFFF",
-    // iconUrl: locationIcon.src,
-  // });
+  const iconMarkup = renderToStaticMarkup(
+    <div style={{backgroundColor: 'transparent'}}>
+      <Image src={locationIcon_bg} alt="point" width={64} height={64}/>
+    </div>
+  );
 
-  // const svgIcon = L.divIcon({
-  //   html: `<svg color="black" fill="none" viewBox="0 0 28 28" class="sc-ipEyDJ jDYzxv"><path fill-rule="evenodd" clip-rule="evenodd" d="M17.85 9.183a5.444 5.444 0 1 0-7.7 7.7l3.3 3.3a.777.777 0 0 0 1.1 0l3.3-3.3a5.444 5.444 0 0 0 0-7.7Zm-8.8-1.1a7 7 0 1 1 9.9 9.9l-3.3 3.3a2.332 2.332 0 0 1-3.3 0l-3.3-3.3a7 7 0 0 1 0-9.9ZM14 11.478a1.556 1.556 0 1 0 0 3.11 1.556 1.556 0 0 0 0-3.11Zm-3.111 1.555a3.111 3.111 0 1 1 6.222 0 3.111 3.111 0 0 1-6.222 0Z"></path></svg>`,
-  //   className: "svg-icon",
-  //   iconSize: [24, 40],
-  //   iconAnchor: [12, 40]
-  // });
-
-  // const pepe = ReactDOMServer.renderToStaticMarkup(<Image alt="location" src={locationIcon} width={28} height={28}/>);
-  // console.log('pepe', pepe);
-
-  function divIcon(icon: string){
-    const markerIcon = L.divIcon({
-      className: 'custom-div-icon',
-      html: `<div style='background-color:red;' class='marker-pin'></div>`,
-      iconSize: [48, 48],
-      iconAnchor: [15, 42],
-    });
-
-    return markerIcon;
-  }
-
-  const svgIcon2 = L.divIcon({
-    html: `<svg
-        color="black"
-        fill="none"
-        viewBox="0 0 28 28"
-        version="1.1"
-        id="svg1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:svg="http://www.w3.org/2000/svg">
-      <path
-          inkscape:original-d="M 0,0"
-          inkscape:path-effect="#path-effect2"
-          d="m 17.85,9.183 a 5.4447222,5.4447222 0 1 0 -7.7,7.7 l 3.3,3.3 a 0.777,0.777 0 0 0 1.1,0 l 3.3,-3.3 a 5.444,5.444 0 0 0 0,-7.7 z"
-          id="path2"
-          style="fill:#ffffff;fill-opacity:1" />
-      <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M17.85 9.183a5.444 5.444 0 1 0-7.7 7.7l3.3 3.3a.777.777 0 0 0 1.1 0l3.3-3.3a5.444 5.444 0 0 0 0-7.7Zm-8.8-1.1a7 7 0 1 1 9.9 9.9l-3.3 3.3a2.332 2.332 0 0 1-3.3 0l-3.3-3.3a7 7 0 0 1 0-9.9ZM14 11.478a1.556 1.556 0 1 0 0 3.11 1.556 1.556 0 0 0 0-3.11Zm-3.111 1.555a3.111 3.111 0 1 1 6.222 0 3.111 3.111 0 0 1-6.222 0Z"
-          id="path1"
-          style="fill:#000000;fill-opacity:1" />
-    </svg>`,
-    className: "",
-    iconSize: [48, 48],
-    iconAnchor: [12, 40],
+  const customMarkerIcon = L.divIcon({
+    html: iconMarkup,
+    className: '',
+    iconSize: [64, 64],
+    iconAnchor: [32, 32],
   });
 
   return (
@@ -86,7 +47,7 @@ const Map = ({latitude, longitude, marker}: IMapProps) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       { marker &&
-        <Marker icon={svgIcon2} position={[latitude, longitude]}>
+        <Marker icon={customMarkerIcon} position={[latitude, longitude]}>
           <Popup>
             A marker at ({latitude}, {longitude})
           </Popup>
